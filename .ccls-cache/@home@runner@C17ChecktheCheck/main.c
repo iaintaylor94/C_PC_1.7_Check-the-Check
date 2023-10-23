6a1ccd4a5bb9170d65bf8aa63d267818a8b38001
Iain Taylor <iain.taylor.94@gmail.com>
2023-10-23 14:41:36 +0000

    completed file I/O

--- .ccls-cache/@home@runner@C17ChecktheCheck/main.c
+++ .ccls-cache/@home@runner@C17ChecktheCheck/main.c
@@ -1,6 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <sys/errno.h>
 
-int main(void) {
-  printf("Hello World\n");
+// Declare variables
+FILE *gInputFile;
+
+// Declare Functions
+void usage (char *);
+
+int main(int argc, char *argv[]) {
+
+  // File Initialization
+  if (argc != 2) usage (argv[0]);
+
+  if (NULL == (gInputFile = fopen(argv[1], "r"))) {
+    fprintf (stderr, "inputFile: %s: %s\n", argv[1], strerror(errno));
+    exit (EXIT_FAILURE);
+  }
+  else {
+    fprintf (stderr, "%s opened for reading.\n" , argv[1]);
+  }
+
+
+
+
+
+  // Close File
+  fclose (gInputFile);
+  
   return 0;
 }
